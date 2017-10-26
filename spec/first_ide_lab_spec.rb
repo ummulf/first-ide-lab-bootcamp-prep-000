0a76b5246081fd16eda890dfd15e150dc1d3d4ef
Ummul Fatima <ummulrizvi@yahoo.com>
2017-10-26 20:11:18 +0000

    automatically backed up by learn

--- spec/first_ide_lab_spec.rb
+++ spec/first_ide_lab_spec.rb
@@ -1,4 +1,5 @@
-RSpec.describe 'First Lab:' do
+Hello World !
+
   it 'you made an edit to edit-me.txt' do
     file_edited = !File.read("./edit-me.txt").empty?
     expect(file_edited).to be_truthy, "Make sure you have edited the file edit-me.txt"
